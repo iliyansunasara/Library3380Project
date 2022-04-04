a8acc7b57c77d61c836575e7720b4d26b52031d6
christijer <70234891+christijer@users.noreply.github.com>
2022-04-04 17:32:24 -0500

    database connection

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -3,7 +3,7 @@
 $serverName = "libraryprojserv.mysql.database.azure.com"; //localhost
 $dBUsername = "adminuser"; //root
 $dBPassword = "22TYRRL8A8V31810$ "; /""
-$dBName = "libraryprojserv";
+$dBName = "library";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
