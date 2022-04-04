0d1cdeed37fdb834ed7aae6c786112a233d18867
Iliyan Sunasara <iliyansunasara1@gmail.com>
2022-04-04 18:00:55 -0500

    edited dbh-inc file

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -21,10 +21,10 @@ $con = mysqli_init();
 //$conn = mysqli_connect($host, $username, $password, $db_name);
 
 // mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
 // Establish the connection
-mysqli_real_connect($conn, 'libraryprojserv.mysql.database.azure.com', 'adminuser', '22TYRRL8A8V31810$', 'library', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "libraryprojserv.mysql.database.azure.com", "adminuser", "22TYRRL8A8V31810$", "library", 3306, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
 if (mysqli_connect_errno())
