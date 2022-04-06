0af6dfd1b8018c8a900b75e5e6587ae4f0139a0e
Iliyan Sunasara <iliyansunasara1@gmail.com>
2022-04-05 21:34:12 -0500

    still debugging dbh-inc

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -1,11 +1,19 @@
 <?php
 
-$serverName = "libraryprojserv.mysql.database.azure.com"; //localhost
-$dBUsername = "adminuser"; //root
-$dBPassword = "22TYRRL8A8V31810$";
-$dBName = "library";
+// $serverName = "libraryprojserv.mysql.database.azure.com"; //localhost
+// $dBUsername = "adminuser"; //root
+// $dBPassword = "22TYRRL8A8V31810$";
+// $dBName = "library";
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+// $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+// if (!$conn) {
+//    die("Connection failed: ".mysqli_connect_error()); 
+// }
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{includes\DigiCertGlobalRootCA.crt.pem}", NULL, NULL);
+mysqli_real_connect($conn, "libraryprojserv.mysql.database.azure.com", "adminuser", "{22TYRRL8A8V31810$}", "{library}", 3306, MYSQLI_CLIENT_SSL);
 
 if (!$conn) {
    die("Connection failed: ".mysqli_connect_error()); 
