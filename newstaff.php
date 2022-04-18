9c7bd18f5b4e9b5bfbf25e72098548e388db8f18
christijer <70234891+christijer@users.noreply.github.com>
2022-04-18 17:02:29 -0500

    button click

--- newstaff.php
+++ newstaff.php
@@ -4,12 +4,12 @@
 <section class="signup-form">
         <h2>To generate table fill out the fields below:</h2>
         <div class="signup-form-form">
-            <form action ="includes/newstaff-inc.php" method="POST">
+            <form action ="includes/newstaff-inc.php" method="POST" tager="_blank">
                 <h3>Start date:</h3>
                 <input type="date" name="start"><br><br>
                 <h3>End date:</h3>
                 <input type="date" name="end"><br><br>
-                <button onclick=" window.open('http://google.com','_blank') name="submit" class="submit">Run Report</button>
+                <button name="submit" class="submit">Run Report</button>
             </form>
         </div>
 </section>
