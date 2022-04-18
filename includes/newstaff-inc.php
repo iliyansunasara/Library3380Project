0be35abc2c9a694c21caac9394c9647af2b73fca
Iliyan Sunasara <iliyansunasara1@gmail.com>
2022-04-18 16:16:29 -0500

    change on newstaff-inc

--- includes/newstaff-inc.php
+++ includes/newstaff-inc.php
@@ -1,18 +1,17 @@
 <?php
     session_start();
-    if(isset("staffReport") && isset($_SESSION['Admin_id'])){
-        require_once 'includes/dbh-inc.php';
-        require_once 'includes/functions-inc.php';
+    if(isset($_POST['staffReport']) && isset($_SESSION['Admin_id'])){
+        require_once 'dbh-inc.php';
+        require_once 'functions-inc.php';
         $start = $_POST['start'];
-        $end = $_POST['end']);
-        if (checkDatesGood($start, $end) {
+        $end = $_POST['end'];
+        if (checkDatesGood($start, $end)) {
             createNewStaffTable($conn, $start, $end);
         }
         else {
             header("Location: newstaff.php.php?error=startdatebig");
             exit();
-        }
-        
+        } 
     }
     else {
         header("Location: login.php?error=loginpls");
