cacbb3acf219f9e4ad68cb69d30f154c8b5f3c57
christijer <70234891+christijer@users.noreply.github.com>
2022-04-18 00:23:41 -0500

    fines changes

--- includes/functions-inc.php
+++ includes/functions-inc.php
@@ -1152,7 +1152,11 @@
     }
     function updateFines($conn, $UnivID, $Fines) {
         $sql = "UPDATE `users` SET `Fines`='$Fines' WHERE `University_id`= $UnivID;";
-        if (mysqli_query($conn, $sql)) {
+        if ($Fines == 0) {
+            header("location: index.php?error=nofines");
+            exit();
+        }
+        else if (mysqli_query($conn, $sql)) {
             header("location: index.php?error=finesadded");
             exit();
         }
